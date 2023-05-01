eb0d153ab879b78bd36db26e9df5a9b47bdde3c8
Chakradhar <110602847+Chakradharbandaru@users.noreply.github.com>
2023-05-01 21:56:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const jokeEl = document.getElementById("joke");
 const jokeBtn = document.getElementById("jokeBtn");
 
+
 const generateJoke = async () => {
   const config = {
     headers: { Accept: "application/json" },
